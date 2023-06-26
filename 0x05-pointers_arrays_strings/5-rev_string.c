6ec4ebc6a5a020d472123efe8ed4007b083e6f17
Duckity6210 <priscaharmon6210@gmail.com>
2023-06-26 06:21:18 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,44 +9,25 @@
 void rev_string(char *s)
 
 {
-	int i = 0, length;
+	int length, z, half;
 
-	length = _strlen(s) - 1;
-	while (length > i)
+	char temp;
+
+	for (length = 0; s[length] != '\0'; length++)
+
+		;
+
+	z = 0;
+	half = length / 2;
+
+	while (half--)
 	{
-		swap_char(s + length, s + i);
-		i++;
-		length--;
+		temp = s[length - z - 1];
+
+		s[length - z - 1] = s[z];
+
+		s[z] = temp;
+
+		z++;
 	}
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string
- * Return: returns length;
- */
-
-int _strlen(char *s)
-{
-	int count, inc;
-	inc = 0;
-	for (count = 0; s[count] != '\0'; count++)
-		inc++;
-
-	return (inc);
-}
-
-/**
- * swap_char - swap two characters
- * @a: first character
- * @b: second character
- * Return: alwaya 0
- */
-
-void swap_char(char &a, char *b)
-
-{
-	char tmp = *a;
-	*a = *b;
-	*b = tmp;
 }
