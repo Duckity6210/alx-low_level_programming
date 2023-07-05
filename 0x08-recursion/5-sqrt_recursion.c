29a7923c62c7a99a1fe37ad9139fbb8c11a8a49a
Duckity6210 <priscaharmon6210@gmail.com>
2023-07-05 07:50:15 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,30 @@
 #include "main.h"
 
 /**
+ * helperfunction - checks if sqrt of number exists
+ * @num: number
+ * @psqrt: possible sqrt
+ * Return: sqrt of number or -1 for error
+ */
+
+int helperfunction(int num, int psqrt)
+{
+	if ((psqrt * psqrt) == num)
+	{
+		return (psqrt);
+	}
+	else
+	{
+	if ((psqrt * psqrt) > num)
+		return (-1);
+	else
+		return (helperfunction(num, psqrt + 1));
+	}
+}
+
+/**
  * _sqrt_recursion - returns the natural square root of number
- * @n: number to find sqrt of
- * @sqrt: possible sqrt of number
+ * @n: number to find sqrt of.
  * Return: squareroot of n
  */
 
@@ -12,5 +33,5 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else
-		return (squareroot of n);
+		return (helperfunction(n, 0));
 }
