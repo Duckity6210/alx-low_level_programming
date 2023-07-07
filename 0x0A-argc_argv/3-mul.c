5a53052838a7eb7daa6a96a7661b22621da1f0e3
Duckity6210 <priscaharmon6210@gmail.com>
2023-07-07 06:57:02 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
+ *
  * Return: the int converted from the string
  */
-
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
@@ -19,44 +18,31 @@ int _atoi(char *s)
 	digit = 0;
 
 	while (s[len] != '\0')
-
 		len++;
 
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-
 			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
-
 		{
 			digit = s[i] - '0';
-
 			if (d % 2)
-
 				digit = -digit;
-
 			n = n * 10 + digit;
 			f = 1;
-
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-
 				break;
-
 			f = 0;
-
 		}
-
 		i++;
 	}
 
 	if (f == 0)
-
 		return (0);
 
 	return (n);
-
 }
 
 /**
@@ -64,17 +50,14 @@ int _atoi(char *s)
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0 (Success), 1 (error)
+ * Return: 0 (Success), 1 (Error)
  */
-
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
-
 	{
-
 		printf("Error\n");
 		return (1);
 	}
@@ -87,3 +70,4 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
+
