6028a4088c37442d165f213574bdf12c6757fd0d
Duckity6210 <priscaharmon6210@gmail.com>
2023-08-03 02:59:23 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,10 +11,10 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int m;
 
-	if (index < 64)
+	if (index > 63)
 		return (-1);
 
-	m = 1 >> index;
+	m = 1 << index;
 
 	if (*n & m)
 		*n ^= m;
