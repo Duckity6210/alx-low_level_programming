c8c18888375124773c9dd6088c8324745cfd63f7
Duckity6210 <priscaharmon6210@gmail.com>
2023-08-11 07:35:11 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,25 +13,20 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (file == -1)
-		return (-1);
 
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
-		{
-			close(file);
-			return (-1);
-		}
-		close(file);
-		return (1);
 	}
-	else
-	{
-		close(file);
-		return (1);
-	}
-}
 
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
